fcaba70552bbe3702e768f3d2e792d2aae496475
fleth <fleth@users.noreply.github.com>
2017-04-30 05:34:26 +0000

    ignore zero data

--- keras/sample6/rnn_brainwave.py
+++ keras/sample6/rnn_brainwave.py
@@ -25,12 +25,12 @@ def degrees(data):
 
 def make_mini_batch(train_data, index, size_of_mini_batch, size_of_input_data):
     inputs  = np.empty(0)
-    outputs = np.empty((0,2))
+    outputs = np.empty(0)
     for _ in range(size_of_mini_batch):
         inputs_part    = train_data["inputs"][index]
         outputs_part    = train_data["outputs"][index]
         inputs  = np.append(inputs, inputs_part)
-        outputs = np.vstack((outputs, outputs_part))
+        outputs = np.append(outputs, degrees(outputs_part[0]))
     inputs = inputs.reshape(-1, size_of_input_data, 1)
     return (inputs, outputs)
 
@@ -50,6 +50,8 @@ for filename in file_list:
     inputs = (loaddata(filename))
     for line in inputs:
         data = json.loads(line)
+        if int(data["mouse"]["move"][0]) == 0:
+            continue
         brainwaves.append(data["brainwaves"])
         keys.append(data["keys"])
         mouse["move"].append(data["mouse"]["move"])
@@ -58,11 +60,9 @@ for filename in file_list:
 print("brainwaves: %s" % len(brainwaves))
 
 num_of_input_nodes          = 50
-num_of_hidden_nodes         = 150
-num_of_output_nodes         = 2
-num_of_training_epochs      = 1
+num_of_training_epochs      = 50
 size_of_mini_batch          = 100
-size_of_batch               = 50000
+size_of_batch               = 1500
 dropout                     = 0.01
 
 train_data = dict()
@@ -70,7 +70,6 @@ train_data["inputs"] = brainwaves
 train_data["outputs"] = mouse["move"]
 
 data, labels = make_mini_batch(train_data, 0, size_of_batch, num_of_input_nodes)
-print(labels)
 tb_cb = keras.callbacks.TensorBoard(log_dir="/tmp/tensorflow_log", histogram_freq=1)
 callbacks = [tb_cb]
 
@@ -82,17 +81,16 @@ model = Sequential()
 model.add(Conv1D(32, 3, padding="same", input_shape=(num_of_input_nodes, 1)))
 model.add(Conv1D(64, 3, padding="same"))
 model.add(MaxPooling1D())
-model.add(LSTM(64))
-#model.add(Flatten())
+model.add(Flatten())
 model.add(Dense(128))
-model.add(Dense(2, activation="relu"))
+model.add(Dense(1, activation="relu"))
 model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
 
 model.summary()
 
 model.fit(data, labels, nb_epoch=num_of_training_epochs, batch_size=size_of_mini_batch, callbacks=callbacks)
 
-test_data, test_labels = make_mini_batch(train_data, 200, size_of_mini_batch, num_of_input_nodes)
+test_data, test_labels = make_mini_batch(train_data, 300, size_of_mini_batch, num_of_input_nodes)
 score = model.evaluate(test_data, test_labels, batch_size=size_of_mini_batch)
 
 print(score)
