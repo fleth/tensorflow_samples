fd3a21439c7654600d80e63ff51e1f9d22e24588
fleth <fleth@users.noreply.github.com>
2017-05-08 10:21:46 +0000

    categorical_crossentropy

--- keras/sample6/rnn_brainwave.py
+++ keras/sample6/rnn_brainwave.py
@@ -23,16 +23,30 @@ def degrees(data):
         degrees = 360 + degrees
     return degrees % 360
 
-def make_mini_batch(train_data, index, size_of_mini_batch, size_of_input_data):
+def make_data(train_data, size_of_mini_batch, size_of_input_data):
     inputs  = np.empty(0)
     outputs = np.empty(0)
-    for _ in range(size_of_mini_batch):
-        inputs_part    = train_data["inputs"][index]
-        outputs_part    = train_data["outputs"][index]
+    base    = 5000
+    for index in range(size_of_mini_batch):
+        inputs_part    = train_data["inputs"][base + index]
+        outputs_part    = train_data["outputs"][base + index]
         inputs  = np.append(inputs, inputs_part)
-        outputs = np.append(outputs, degrees(outputs_part[0]))
+        tmp = np.zeros(360)
+        tmp[int(degrees(outputs_part[0]))] = 1
+        outputs = np.append(outputs, tmp)
     inputs = inputs.reshape(-1, size_of_input_data, 1)
+    outputs = outputs.reshape(-1, 360)
     return (inputs, outputs)
+
+def make_rand_data(num_of_samples, data, labels):
+    rand_data   = np.empty(0)
+    rand_labels = np.empty(0)
+    for _ in range(num_of_samples):
+        index   = random.randint(0, len(data))
+        rand_data   = np.append(rand_data, data[index])
+        rand_labels = np.append(rand_labels, labels[index])
+    rand_data = rand_data.reshape(num_of_samples, -1, 1)
+    return (rand_data, rand_labels)
 
 keys = []
 brainwaves = []
@@ -44,12 +58,18 @@ file_list = [
     "../data/20170203_223600.json",
     "../data/20170206_214114.json",
     "../data/20170206_223307.json",
-    "../data/20170207_220719.json"]
+    "../data/20170207_220719.json",
+    "../data/20170507_152458.json",
+    "../data/20170507_161822.json",
+    "../data/20170507_204856.json",
+    "../data/20170507_214606.json",
+    "../data/20170507_222431.json"]
 
 for filename in file_list:
     inputs = (loaddata(filename))
     for line in inputs:
         data = json.loads(line)
+        # ignore zero
         if int(data["mouse"]["move"][0]) == 0:
             continue
         brainwaves.append(data["brainwaves"])
@@ -60,16 +80,18 @@ for filename in file_list:
 print("brainwaves: %s" % len(brainwaves))
 
 num_of_input_nodes          = 50
-num_of_training_epochs      = 5
+num_of_output_nodes         = 360
+num_of_training_epochs      = 200
 size_of_mini_batch          = 100
-size_of_batch               = 1500
+size_of_batch               = 5000
 dropout                     = 0.01
 
 train_data = dict()
 train_data["inputs"] = brainwaves
 train_data["outputs"] = mouse["move"]
 
-data, labels = make_mini_batch(train_data, 0, size_of_batch, num_of_input_nodes)
+data, labels = make_data(train_data, size_of_batch, num_of_input_nodes)
+print(labels)
 tb_cb = keras.callbacks.TensorBoard(log_dir="/tmp/tensorflow_log", histogram_freq=1)
 callbacks = [tb_cb]
 
@@ -78,23 +100,22 @@ random.seed(0)
 np.random.seed(0)
 
 model = Sequential()
-model.add(Conv1D(64, 3, padding="same", input_shape=(num_of_input_nodes, 1)))
-model.add(Conv1D(128, 3, padding="same"))
+model.add(Conv1D(128, 3, padding="same", input_shape=(num_of_input_nodes, 1)))
+model.add(Conv1D(128, 3, padding="same", activation="relu"))
 model.add(MaxPooling1D())
 model.add(Flatten())
+#model.add(LSTM(64))
 model.add(Dense(128, activation="relu"))
-model.add(Dense(1, activation="relu"))
-model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+model.add(Dense(num_of_output_nodes, activation="softmax"))
+optimizer   = "adadelta"
+model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
 
 model.summary()
 
-model.fit(data, labels, nb_epoch=num_of_training_epochs, batch_size=size_of_mini_batch, callbacks=callbacks)
+model.fit(data, labels, nb_epoch=num_of_training_epochs, batch_size=size_of_mini_batch, callbacks=callbacks, validation_split=0.2)
 
-test_data, test_labels = make_mini_batch(train_data, 300, size_of_mini_batch, num_of_input_nodes)
-score = model.evaluate(test_data, test_labels, batch_size=size_of_mini_batch)
-
-print(score)
-
+test_data, test_labels = make_rand_data(20, data, labels)
+print("%s, %s, %s, %s" % (data.shape, labels.shape, test_data.shape, test_labels.shape))
 predict = model.predict(test_data, batch_size=size_of_mini_batch)
 
 print(predict)
