0f29d917a60923ec09a19f4cc9cd03a77f28af73
fleth <fleth@users.noreply.github.com>
2017-05-09 09:12:46 +0000

    to categorical

--- keras/sample6/rnn_brainwave.py
+++ keras/sample6/rnn_brainwave.py
@@ -33,10 +33,12 @@ def make_data(train_data, size_of_mini_batch, size_of_input_data, concat):
         inputs_part    = train_data["inputs"][i:i+concat]
         outputs_part    = train_data["outputs"][i+concat]
         inputs  = np.vstack((inputs, inputs_part.reshape(-1)))
-        tmp = np.zeros(360)
-        tmp[int(degrees(outputs_part[0]))] = 1
-        outputs = np.vstack((outputs, tmp))
+        outputs = np.append(outputs, int(degrees(outputs_part[0])))
     inputs = inputs.reshape(-1, size_of_input_data * concat, 1)
+    print(outputs[0])
+    outputs = keras.utils.to_categorical(outputs, 360)
+    print(outputs[0])
+    print("inputs: %s, outputs: %s" % (inputs.shape, outputs.shape))
     return (inputs, outputs)
 
 def make_rand_data(num_of_samples, data, labels):
@@ -48,6 +50,7 @@ def make_rand_data(num_of_samples, data, labels):
         rand_data   = np.vstack((rand_data, data[index]))
         rand_labels = np.vstack((rand_labels, labels[index]))
     rand_data = rand_data.reshape(num_of_samples, -1, 1)
+    print("rand_data: %s, rand_labels: %s" % (rand_data.shape, rand_labels.shape))
     return (rand_data, rand_labels)
 
 keys = []
@@ -87,20 +90,18 @@ mouse["button"] = np.asarray(mouse["button"])
 print("brainwaves: %s" % len(brainwaves))
 print("channel first: %s" % K.image_data_format())
 
-concat                      = 4
+concat                      = 2
 num_of_input_nodes          = 50
 num_of_output_nodes         = 360
-num_of_training_epochs      = 5
+num_of_training_epochs      = 50
 size_of_mini_batch          = 100
 size_of_batch               = 500
-dropout                     = 0.01
 
 train_data = dict()
 train_data["inputs"] = brainwaves
 train_data["outputs"] = mouse["move"]
 
 data, labels = make_data(train_data, size_of_batch, num_of_input_nodes, concat)
-print("%s, %s" % (data.shape, labels.shape))
 tb_cb = keras.callbacks.TensorBoard(log_dir="/tmp/tensorflow_log", histogram_freq=1)
 callbacks = [tb_cb]
 
@@ -109,14 +110,14 @@ random.seed(0)
 np.random.seed(0)
 
 model = Sequential()
-model.add(Conv1D(32, 3, padding="same", input_shape=(num_of_input_nodes * concat, 1)))
-model.add(Conv1D(64, 3, padding="same", activation="relu"))
+model.add(Conv1D(64, 3, padding="same", input_shape=(num_of_input_nodes * concat, 1)))
+model.add(Conv1D(128, 3, padding="same", activation="relu"))
 model.add(MaxPooling1D())
-model.add(Dropout(0.25))
-model.add(Flatten())
-#model.add(LSTM(64))
-model.add(Dense(128, activation="relu"))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.25))
+#model.add(Flatten())
+model.add(LSTM(128))
+model.add(Dense(256, activation="relu"))
+#model.add(Dropout(0.5))
 model.add(Dense(num_of_output_nodes, activation="softmax"))
 model.compile(optimizer=keras.optimizers.Adadelta(), loss="categorical_crossentropy", metrics=["accuracy"])
 
@@ -125,7 +126,6 @@ model.summary()
 model.fit(data, labels, nb_epoch=num_of_training_epochs, batch_size=size_of_mini_batch, callbacks=callbacks, validation_split=0.2)
 
 test_data, test_labels = make_rand_data(20, data, labels)
-print("%s, %s, %s, %s" % (data.shape, labels.shape, test_data.shape, test_labels.shape))
 predict = model.predict(test_data, batch_size=size_of_mini_batch)
 
 print("predict: %s" % predict)
